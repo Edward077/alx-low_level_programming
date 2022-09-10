f1e9e23b074c94856ac572ac252c64bc3d3980fd
Edward077 <edwardthoronka077@gmail.com>
2022-09-10 00:17:32 -0700

    task 3 again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,7 @@
  * this program print "programming is like building a multilingual puzzle
  * Return: 0
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a price of art,\n");
